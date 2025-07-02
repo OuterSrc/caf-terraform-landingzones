1d9f2468e5d96a54e3cf72536993c6ae01f43f0e
Prakash Paruchuri <prakash.paruchuri@microland.com>
2025-07-02 16:22:45 +0100

    version

--- caf_solution/main.tf
+++ caf_solution/main.tf
@@ -20,7 +20,7 @@ terraform {
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "~> 1.2.0"
+      version = "~> 1.2.0, !=1.2.30"
     }
   }
   required_version = ">= 1.3.5"
